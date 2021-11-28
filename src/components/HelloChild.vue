e863a7bcf94b560cb47b8a7ec78b8c524597f85e
akki-to <yanach1n919@gmail.com>
2021-11-29 00:31:04 +0900

    kk

---
+++ src/components/HelloChild.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>{{title}}</p>
+        <p>{{message}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+            title: 'ハロー子供'
+        }
+    },
+    props: ['message']
+}
+</script>
