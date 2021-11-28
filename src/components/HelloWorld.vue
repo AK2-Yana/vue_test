d54d67581151b59cbbfcd3b53e50e625d57fa930
akki-to <yanach1n919@gmail.com>
2021-11-29 00:49:24 +0900

    aaa

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,23 @@
 <template>
-  <div>
-    <p>{{title}}</p>
-    <hello-child message='メッセージだよ'/>
-  </div>
+ <div>
+  <p>{{title}}</p>
+  <!-- v-model を使ってinput_msgにinputタグで入力した値を都度反映する -->
+  <p><input v-model="input_msg"></p>
+  <!-- 子コンポーネントのmessageに反映した値をを設定して呼び出す -->
+  <hello-child :message="input_msg"/>
+ </div>
 </template>
-
+ 
 <script>
 import HelloChild from './HelloChild'
-
 export default {
-  data: function(){
+  data: function () {
     return {
-      title: 'ハロー！ワールド！'
+      title: 'はろー！わーるど！',
+      input_msg: ''
     }
   },
-  components:{
+  components: {
     HelloChild
   }
 }
